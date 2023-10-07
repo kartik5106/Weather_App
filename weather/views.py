01e0d00c85c4b474efff305c985f22d095ca0132
kartik5106 <kartikrr5106@gmail.com>
2023-10-07 11:18:43 +0530

    added sample.html and trying out django forms

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,17 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'weather.html')
+    city = "mangalore"
+    myApi= '05c506850f2a4c7f868101120231309'
+    url =f'http://api.weatherapi.com/v1/current.json?key={myApi}&q={city}&aqi=no'
+    r = requests.get(url).json()
+    
+    city_weather = {
+        'city': city,
+        'temperature': r['current']['temp_c'],
+        'description':r['current']['condition']['text']
+    }
+    context = {'city_weather' : city_weather}
+    return render(request,'sample.html',context)
